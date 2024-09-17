212fbaf350b347bd6bd4242c456894a035349465
Marlon Marin <marlonmariin97@outlook.com>
2024-09-16 21:57:26 -0500

    (new) pca.py > This script retrieves the values of explained variance through 3 component PCA and incremental PCA model for a 14-dimensional dataset. (new) kpca.py > It does the same as pca.py but applies a kernelized version of PCA by applying a polynomial surface adjust, and retrieves explained variance through 4 components. Both pcas are conducted to be used in a Logistic Regression model (binary classification of heart disease).

--- kpca.py
+++ kpca.py
@@ -3,7 +3,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA #type:ignore
-from sklearn.decomposition import IncrementalPCA #type:ignore
+from sklearn.decomposition import KernelPCA #type:ignore
 from sklearn.linear_model import LogisticRegression #type:ignore
 from sklearn.preprocessing import StandardScaler #type:ignore
 from sklearn.model_selection import train_test_split #type:ignore
@@ -12,7 +12,6 @@ matplotlib.use('Agg')  # Forces Matplotlib to run in non-GUI mode
 
 if __name__ == "__main__":
     df_heart = pd.read_csv('./data/heart.csv')
-    print(df_heart.shape)
     
     # Features
     df_features = df_heart.drop(['target'], axis=1)
@@ -22,30 +21,13 @@ if __name__ == "__main__":
     
     # Train
     X_train, X_test, y_train, y_test = train_test_split(df_features, df_target, test_size=0.3, random_state=42)
+    kpca = KernelPCA(n_components=4, kernel='poly')
+    kpca.fit(X_train)
     
-    # n_components = min(n_samples, n_features)
-    pca = PCA(n_components=3)
-    pca.fit(X_train)
-    
-    ipca = IncrementalPCA(n_components=3, batch_size=10)
-    ipca.fit(X_train)
-    
-    x = range(len(pca.explained_variance_)) # Three components already defined
-    y = pca.explained_variance_ratio_
-    plt.plot(x, y)
-    plt.xlabel('n_components')
-    plt.ylabel('variance ratio')
-    plt.savefig('./pca.png')
+    # Test
+    df_train = kpca.transform(X_train)
+    df_test = kpca.transform(X_test)
     
     logistic = LogisticRegression(solver='lbfgs', random_state=42)
-    
-    # Applying PCA using logistic as predicting model
-    df_train = pca.transform(X_train)
-    df_test = pca.transform(X_test)
     logistic.fit(df_train, y_train)
-    print("Score PCA: ", logistic.score(df_test, y_test))
-    
-    df_train = ipca.transform(X_train)
-    df_test = ipca.transform(X_test)
-    logistic.fit(df_train, y_train)
-    print("Score IPCA:", logistic.score(df_test, y_test))
+    print("Score KPCA: ", logistic.score(df_test, y_test))
